9a48051ebc63588e014f430990ae869aeb25c787
Melipilla <melipilla@duoc.acad>
2025-05-09 17:38:44 -0400

    Sucursales

---
+++ src/test/java/com/perfurlandia/Sucursales/SucursalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfurlandia.Sucursales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SucursalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
